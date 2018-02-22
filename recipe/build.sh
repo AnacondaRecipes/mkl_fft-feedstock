cbc7f8bbead8b4fc910a8d54c1fd2f6f23297a63
Stuart Archibald <stuart.archibald@googlemail.com>
2018-02-22 12:10:36 +0000

    Initial cut of mkl_fft recipe.
    As title.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 #!/bin/bash -x
 
 # make sure that compiler has been sourced, if necessary
 
 CFLAGS="-I$PREFIX/include $CFLAGS" $PYTHON setup.py build --force install --old-and-unmanageable
 
+=======
+#!/bin/bash
+MACOSX_DEPLOYMENT_TARGET=10.9 $PYTHON setup.py build install --single-version-externally-managed --record=record.txt
+>>>>>>> Initial cut of mkl_fft recipe.
